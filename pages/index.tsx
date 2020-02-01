7140a0032b9acd98a7d00a269c6a56fc64378ef6
Keith Pounds <KPounds@gmcps.com>
2020-01-31 20:11:57 -0600

    removing unused file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
 import { NextPage } from "next"
-import Link from "next/link"
 
 const Home: NextPage = () => (
   <div>
-    <Link href="/about">
-      <a>about page</a>
-    </Link>
     <p>Hello next.js</p>
   </div>
 )
